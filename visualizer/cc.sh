f82ec9c8bbd786dea98da1ff2e6a1f70fe5c65fb
Gautier LEMAIRE <galemair@e2r2p6.42.fr>
2018-06-19 19:41:27 +0200

    Starting visualizer implementation

---
+++ visualizer/cc.sh
@@ -0,0 +1,1 @@
+gcc visualizer.c -I inc -I ~/.brew/Cellar/sdl2/2.0.8/include/SDL2/ -L ~/.brew/Cellar/sdl2/2.0.8/lib/ -lSDL2
---
+++ visualizer/inc/const.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   const.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: galemair <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/15 17:02:07 by galemair          #+#    #+#             */
+/*   Updated: 2018/06/19 19:26:55 by galemair         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONST_H
+# define CONST_H
+
+#define WINDOW_NAME "Lem_In"
+#define WINDOW_WIDTH 640
+#define WINDOW_LENGTH 480
+#define	TRUE 1
+#define	FALSE 0
+
+
+#endif
