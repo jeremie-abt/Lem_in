74fea4778917be3d89175107dc76f337a8a8dcdd
Gautier LEMAIRE <galemair@e2r12p19.42.fr>
2018-06-20 14:15:24 +0200

    salut

---
+++ debug.sh
@@ -0,0 +1,1 @@
+gcc -g -fsanitize=address src/*.c src/algo/*.c libft/libft.a lem_in.c -I inc/ -I libft/includes
--- src/algo/lm_start_algo.c
+++ src/algo/lm_start_algo.c
@@ -6,12 +6,20 @@
 /*   By: galemair <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/05 10:58:09 by galemair          #+#    #+#             */
-/*   Updated: 2018/06/20 12:47:00 by galemair         ###   ########.fr       */
+/*   Updated: 2018/06/20 14:12:57 by galemair         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 
+void			print_queue(t_sommet *lst)
+{
+	while (lst)
+	{
+		printf("NAME : %s et DIS : %d\n", lst->name, lst->distance);
+		lst = lst->queue;
+	}
+}
 void			lm_queue_neighbor(t_sommet *head, t_sommet **queue, t_sommet **sommet)
 {
 	t_sommet	*tmp;
@@ -28,6 +36,8 @@ void			lm_queue_neighbor(t_sommet *head, t_sommet **queue, t_sommet **sommet)
 		}
 		adj_lst = adj_lst->next;
 	}
+	print_queue(*queue);
+	printf("----------\n");
 }
 
 int			get_distance(t_sommet **sommet, t_sommet **queue)
